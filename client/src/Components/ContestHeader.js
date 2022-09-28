c47de51666a10d8640bf18b6daa7cc6c0a4e5d27
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2022-09-28 16:37:25 +0530

    making contest page responsive

--- client/src/Components/ContestHeader.js
+++ client/src/Components/ContestHeader.js
@@ -5,8 +5,8 @@ const ContestHeader = () => {
   const { contest } = useParams();
   const plateform = contest || "all";
   return (
-    <div className="w-full h-28 flex justify-center items-center">
-      <h1 className="text-5xl font-light text-white p-4 font-NavbarLink dark:text-[#6F7684]">
+    <div className="w-full sm:h-28  flex justify-center items-center flex-wrap">
+      <h1 className="lg:text-5xl md:text-4xl sm:text-3xl text-3xl font-light text-white p-4 font-NavbarLink dark:text-[#6F7684]">
         Running and Upcoming Contest of {plateform}
       </h1>
     </div>
